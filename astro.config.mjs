ed9bbc4d5460cd1850e0cef7950e9ef103ae6a48
Harinn <prinn.dev@pm.me>
2026-03-09 02:30:33 +0700

    fix: astro config for deploy on github pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,8 @@ import react from "@astrojs/react"
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://miniharinn.github.io",
+  base: "/r-ryantm-orbit",
   vite: {
     plugins: [tailwindcss()],
   },
