1082f955f60640326d3fddb1226aa7782f1f449c
Harinn <prinn.dev@pm.me>
2026-03-10 20:08:22 +0700

    fix: export CarouselApi to keep TypeScript happy

--- src/components/ui/carousel.tsx
+++ src/components/ui/carousel.tsx
@@ -7,7 +7,7 @@ import { cn } from "@/lib/utils"
 import { Button } from "@/components/ui/button"
 import { IconChevronLeft, IconChevronRight } from "@tabler/icons-react"
 
-type CarouselApi = UseEmblaCarouselType[1]
+export type CarouselApi = UseEmblaCarouselType[1]
 type UseCarouselParameters = Parameters<typeof useEmblaCarousel>
 type CarouselOptions = UseCarouselParameters[0]
 type CarouselPlugin = UseCarouselParameters[1]
