ac20dcf0eba22faab06eb632521240ff61128b89
Harinn <prinn.dev@pm.me>
2026-04-13 01:20:52 +0700

    fix: attempt to fix baseurl

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,7 +8,7 @@ import sitemap from "@astrojs/sitemap"
 // https://astro.build/config
 export default defineConfig({
   site: "https://r-ryantm.harinn.dev",
-  base: "/",
+  // base: "/",
   vite: {
     plugins: [tailwindcss()],
   },
